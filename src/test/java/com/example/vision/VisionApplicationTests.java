481f819882c3b6c46deb31fb28d0b30ad9401e2b
Zehra Uğurlu <zeugurlu@outlook.com>
2024-09-02 14:02:29 +0300

    Initial commit

---
+++ src/test/java/com/example/vision/VisionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vision;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VisionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
